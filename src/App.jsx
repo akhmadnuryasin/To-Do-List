d91be1cbe73e353bfbb065cff98a7ce4936f20b1
Akhmad Nuryasin <akhmadnuryasin18@gmail.com>
2023-12-09 19:33:09 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" />
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
