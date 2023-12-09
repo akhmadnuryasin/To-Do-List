f3ee0d86ed435b0f9f0fe932b9ff670e428a61f5
Akhmad Nuryasin <akhmadnuryasin18@gmail.com>
2023-12-09 19:57:19 +0700

    finish To-Do App project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [listItem, setListItems] = useState([]);
+
+  function handleChange(e) {
+    const newValue = e.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setListItems((prevValue) => {
+      return [...prevValue, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={inputText} />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul></ul>
+        <ul>
+          {listItem.map((todoItem, index) => (
+            <li key={index}>{todoItem}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
